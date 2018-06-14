d5639ed5cf7850201b1ba130ab245dee95aeda69
mkbzowski <33320351+mkbzowski@users.noreply.github.com>
2018-06-14 15:09:38 +0200

    Add files via upload

---
+++ Project10 (1)/src/main/java/com/balaga/Project10/PathProperties.java
@@ -0,0 +1,18 @@
+package com.balaga.Project10;
+
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("img.pictures")
+public class PathProperties {
+    String path;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
